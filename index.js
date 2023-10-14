0cb6359c7b12e45069843bdb6b745848e6155e8e
Nziza21 <144722415+Nziza21@users.noreply.github.com>
2023-10-14 17:48:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your classes here
+class Tree {
+  constructor(species) {
+    this.species = species;
+  }
+
+  static definition() {
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.";
+  }
+}
+
+class Deciduous extends Tree {
+  constructor(species, name) {
+    super(species);
+    this.name = name;
+  }
+
+  static definition() {
+    return `${super.definition()} Deciduous trees shed their leaves annually.`;
+  }
+}
+
+class Evergreen extends Tree {
+  constructor(species, name) {
+    super(species);
+    this.name = name;
+  }
+
+  static definition() {
+    return `${super.definition()} Evergreens keep their leaves all year round.`;
+  }
+}
+const genericTree = new Tree("Generic Tree");
+console.log(genericTree.species); // "Generic Tree"
+console.log(Tree.definition());
+
+const oakTree = new Deciduous("Oak", "Oak Tree");
+console.log(oakTree.species); // "Oak"
+console.log(oakTree.name); // "Oak Tree"
+console.log(Deciduous.definition());
+
+const pineTree = new Evergreen("Pine", "Pine Tree");
+console.log(pineTree.species); // "Pine"
+console.log(pineTree.name); // "Pine Tree"
+console.log(Evergreen.definition());
